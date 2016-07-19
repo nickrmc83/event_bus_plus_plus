82e01620de9d7b85b1d591ff5492e8df8177f4be
Nick A. Smith <nickrmc83@gmail.com>
2016-07-19 07:15:15 +0100

    Tidying code. Code lines now <= 80 characters.

--- include/event_bus.hpp
+++ include/event_bus.hpp
@@ -1,5 +1,6 @@
 /**
- * This source code is governed by by the boost v1 licensing terms and conditions.
+ * This source code is governed by by the boost v1 licensing terms and
+ * conditions.
  * Copyright 2016 Nicholas A. Smith (nickrmc83@gmail.com)
  */
 #include <algorithm>
@@ -103,7 +104,8 @@ namespace events
     class event_bus
     {
       private:
-        typedef std::vector<std::shared_ptr<event_subscriber_base>> subscribers_type;
+        typedef std::vector<std::shared_ptr<event_subscriber_base>>
+          subscribers_type;
         typedef subscribers_type::iterator subscribers_iterator;
         std::map<std::type_index, subscribers_type> event_subscriber_map;
         publish_strategy strategy;
@@ -127,10 +129,13 @@ namespace events
       public:
 
         template<typename event>
-          event_bus &subscribe(std::shared_ptr<event_subscriber<event>> &subscriber)
+          event_bus &subscribe(
+              std::shared_ptr<event_subscriber<event>> &subscriber)
           {
-            auto &subscribers = event_subscriber_map[std::type_index(typeid(event))];
-            auto where = std::find(subscribers.begin(), subscribers.end(), subscriber);
+            auto &subscribers = 
+              event_subscriber_map[std::type_index(typeid(event))];
+            auto where =
+              std::find(subscribers.begin(), subscribers.end(), subscriber);
             if(where != subscribers.end())
             {
               throw already_subscribed_exception::create_exception(subscriber);
@@ -142,16 +147,19 @@ namespace events
         template<typename event>
           event_bus &subscribe(event_subscriber<event> *subscriber)
           {
-            // Subscribe using a smart_pointer that will not delete the pointer.
+            // Subscribe using a smart_pointer that will not delete the pointer
             auto shared = get_shared(subscriber);
             return subscribe(shared);
           }
 
         template<typename event>
-          event_bus &unsubscribe(const std::shared_ptr<event_subscriber<event>> &subscriber)
+          event_bus &unsubscribe(const std::shared_ptr<event_subscriber<event>>
+              &subscriber)
           {
-            auto &subscribers = event_subscriber_map[std::type_index(typeid(event))];
-            auto where = std::find(subscribers.begin(), subscribers.end(), subscriber);
+            auto &subscribers =
+              event_subscriber_map[std::type_index(typeid(event))];
+            auto where =
+              std::find(subscribers.begin(), subscribers.end(), subscriber);
             if(where != subscribers.end())
             {
               subscribers.erase(where);
@@ -170,7 +178,8 @@ namespace events
         template<typename event>
           event_bus &publish(const event &ev)
           {
-            auto &subscribers = event_subscriber_map[std::type_index(typeid(event))];
+            auto &subscribers =
+              event_subscriber_map[std::type_index(typeid(event))];
             event_subscriber_list_t<event> publish_to;
             for(subscribers_iterator subscriber = subscribers.begin();
                 subscriber != subscribers.end();
